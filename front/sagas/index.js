ae6ccc31599891c9d5d0d0c39d1579801dcb8aca
jongju-Lee <jeje7@naver.com>
2023-11-01 00:25:18 +0900

    saga 쪼개고 reducer와 연결하기

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -1,37 +1,7 @@
-import { all, fork, take, call, put } from "redux-saga/effects";
-import axios from "axios";
-
-function logInAPI() {
-  return axios.post("/api/login");
-}
-
-function* logIn() {
-  try {
-    const result = yield call(logInAPI);
-    yield put({
-      type: "LOG_IN_SUCCESS",
-      data: result.data,
-    });
-  } catch (err) {
-    yield put({
-      type: "LOG_IN_FAILURE",
-      data: err.response.data,
-    });
-  }
-}
-
-function* watchLogIn() {
-  yield take("LOG_IN_REQUEST", logIn);
-}
-
-function* watchLogOut() {
-  yield take("LOG_OUT_REQUEST");
-}
-
-function* watchAddPost() {
-  yield take("ADD_POST_REQUEST");
-}
+import { all, fork } from "redux-saga/effects";
+import postSaga from "./post";
+import userSaga from "./user";
 
 export default function* rootSaga() {
-  yield all([fork(watchLogIn), fork(watchLogOut), fork(watchAddPost)]);
+  yield all([fork(postSaga), fork(userSaga)]);
 }
